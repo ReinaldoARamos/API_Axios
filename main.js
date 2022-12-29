c2d21b04d812d09dcf4a1e3c2f53c7cf0ca7af1c
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2022-12-29 16:51:20 -0300

    passando para o DOM o objeto

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ const url = "http://localhost:5500/api"
 function getUser(){
 
     axios.get(url)
-    .then(response => console.log(response))
+    .then(response => {
+        renderAPIData.textContent = JSON.stringify(response.data)
+    })
+    
     .catch(error => console.log(error))
 
     }
